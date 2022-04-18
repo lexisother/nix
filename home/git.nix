214a92f253e0dbf38acd230803525ebb7c4cac8d
Alyxia Sother <alyxia@riseup.net>
2022-04-18 14:28:07 +0200

    [home/git] Enable delta

--- home/git.nix
+++ home/git.nix
@@ -6,6 +6,15 @@
     userName = "Alyxia Sother";
     userEmail = "alyxia@riseup.net";
 
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        features = "decorations";
+        syntax-theme = "ansi";
+      };
+    };
+
     extraConfig = {
       init.defaultBranch = "master";
     };
